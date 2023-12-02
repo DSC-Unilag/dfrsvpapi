ed9fcf9246133bfc92e3e4a0ca2817e77665da75
curiouspaul1 <asalupaul36@gmail.com>
2023-12-02 11:39:36 +0100

    updated models for venue

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from extensions import db
 import sqlalchemy as sa
+import random
 
 
 registrations = db.Table(
@@ -17,6 +18,19 @@ registrations = db.Table(
         primary_key=True
     )
 )
+
+def generate_token(length, special_chars=False, upper_case_only=True):
+    chars = "abcdefghijkmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ0123456789"
+
+    if special_chars:
+        chars += "~!@#$%^&*()?+_-[]{};><"
+
+    token = "".join(random.choice(chars) for x in range(length))
+
+    if upper_case_only:
+        token = token.upper()
+
+    return token
 
 
 class Attendees(db.Model):
@@ -42,6 +56,7 @@ class Event(db.Model):
     speaker = db.Column(db.String())
     speaker_bio = db.Column(db.Text)
     time = db.Column(db.String)
+    venue_id = db.Column(db.String(), db.ForeignKey('venue.venue_id'))
 
 
     @staticmethod
@@ -50,39 +65,47 @@ class Event(db.Model):
             {
                 "title": "Introduction to GCP: A Guide to Google\'s Cloud-9",
                 "speaker": "Oluwagbemileke Kolawole - Cloud Enginner",
-                "time": ""
+                "venue_id": "5VY1UVM83P"
             },
             {
                 "title": "Design and Motion: The Two Infinity Stones of Memorable User Experiences",
-                "speaker": "Chinaza Icheke - UI/UX Designer"
+                "speaker": "Chinaza Icheke - UI/UX Designer",
+                "venue_id": "JUZGQ3JWAY"
             },
             {
                "title": "Transitioning from REST to GraphQL: Enhancing API Efficiency",
-               "speaker": "Adejoke Haastrup - Software Engineer"
+               "speaker": "Adejoke Haastrup - Software Engineer",
+               "venue_id": "3XJXRHFH6D"
             },
             {
                 "title": "Web3 for Web2 Developers",
-                "speaker": "Idris Olubisi - Developer Advocate, Axelar"
+                "speaker": "Idris Olubisi - Developer Advocate, Axelar",
+                "venue_id": "5VY1UVM83P"
             },
             {
                 "title": "Unveiling the Power Within: Mastering Google Chrome DevTools for Web Excellence.",
-                "speaker": "Jesus Akanle - Content Lead (GDSC Unilag)"
+                "speaker": "Jesus Akanle - Content Lead (GDSC Unilag)",
+                "venue_id": "JUZGQ3JWAY"
             },
             {
                 "title": "Leveraging User psychology for Product Growth",
-                "speaker": "TGoodness Ehizode - Product Leader"
+                "speaker": "TGoodness Ehizode - Product Leader",
+                "venue_id": "3XJXRHFH6D"
             },
             {
                 "title": "Turbocharge Your Angular Applications: The Power of Real-time with Angular Signals",
-                "speaker": "Olayinka Atobiloye - GitHub Campus Expert"
+                "speaker": "Olayinka Atobiloye - GitHub Campus Expert",
+                "venue_id": "5VY1UVM83P"
             },
             {
                 "title": "Navigating the Feasibility and Future Trends of Machine Learning Integration in Flutter Applications",
-                "speaker": "Ogbonna Emmanuella - Flutter Developer"
+                "speaker": "Ogbonna Emmanuella - Flutter Developer",
+                "venue_id": "JUZGQ3JWAY"
             },
             {
                 "title": "LangChain and LLMs: Building your own generation AI chatbot trained on your data with LangChain",
-                "speaker": "Salim Oyinlola - Gold Microsoft Ambassador"
+                "speaker": "Salim Oyinlola - Gold Microsoft Ambassador",
+                "venue_id": "3XJXRHFH6D"
             }
         ]
 
@@ -97,6 +120,7 @@ class Event(db.Model):
                 title=event['title'],
                 session_id=grp,
                 speaker=event['speaker'],
+                venue_id=event['venue_id'],
                 time=time_mp[grp]
             )
             db.session.add(new)
@@ -110,4 +134,30 @@ class Event(db.Model):
         finally:
             db.session.close()
         
+        return 'ok'
+
+
+class Venue(db.Model):
+    venue_id = db.Column(db.String(10), primary_key=True)
+    events = db.relationship('Event', backref='venue')
+
+    @staticmethod
+    def create_venues():
+        ids = [
+            generate_token(
+                length=10,
+                upper_case_only=True
+            ) for _ in range(3)
+        ]
+        for tk in ids:
+            new_venue = Venue(venue_id=tk)
+            db.session.add(new_venue)
+        try:
+            db.session.commit()
+        except Exception as e:
+            print(str(e))
+            return 'not ok'
+        finally:
+            db.session.close()
+
         return 'ok'
